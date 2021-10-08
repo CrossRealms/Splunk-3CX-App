a19d578531eb0f28570917a3b06af94c9ece289a
Vatsal Jagani <vatsaljagani85@gmail.com>
2021-10-08 20:59:17 +0530

    Recording button issues fixed and tested on call logs dashboard.

--- appserver/static/recordingbtn.js
+++ appserver/static/recordingbtn.js
@@ -13,11 +13,20 @@ require([
         },
         render: function($td, cell) {
             // Create the button with the URL to put in the cell
-	    if(cell.value === 'NOREC') {
-               $td.html('No recording found');
-	    } else {
-               $td.html('<a class="btn" href='+cell.value+'>Play Recording</a>');
-	    }
+            if(cell.value === null) {
+                $td.html('');
+            } else {
+                if(cell.value instanceof Array){
+                    let inner_html = '<a target="_blank" class="btn btn-primary" href='+cell.value[0]+'>Play Recording 1</a><br/>';
+                    for(let i=1; i<cell.value.length; i++){
+                        inner_html += cell.value[i]+'<br/>';
+                    }
+                    $td.html(inner_html);
+                }
+                else{
+                    $td.html('<a target="_blank" class="btn btn-primary" href='+cell.value+'>Play Recording</a>');
+                }
+            }
         }
     });
     mvc.Components.get('callrep').getVisualization(function(tableView){
